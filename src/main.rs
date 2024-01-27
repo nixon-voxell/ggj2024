41525eecacb76866b6050cdbedfbd0da46693e49
Kah Boon <changkahboon25@gmail.com>
2024-01-28 02:02:19 +0800

    feat: add random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
 use bevy_motiongfx::prelude::*;
 use bevy_rapier2d::prelude::*;
-use motiongfx_typst::{TypstCompiler, TypstCompilerPlugin};
-use motiongfx_vello::{bevy_vello_renderer::vello::peniko, svg};
+// use motiongfx_typst::{TypstCompiler, TypstCompilerPlugin};
+// use motiongfx_vello::{bevy_vello_renderer::vello::peniko, svg};
+use bevy::prelude::ResMut;
+// use bevy_prng::ChaCha8Rng;
+// use bevy_rand::prelude::*;
+// use rand_core::RngCore;
+// mod board;
+use rand::{rngs::ThreadRng, Rng};
 
-mod board;
 mod emoji;
 mod emoji_ui;
 mod game;
@@ -32,7 +37,14 @@ fn main() {
         // Systems
         .add_systems(Startup, setup)
         .add_systems(Startup, menu_ui::menu_button)
-        .add_systems(Startup, (emoji_ui::setup, emoji_ui::setup_menu))
+        .add_systems(
+            Startup,
+            (
+                emoji_ui::setup,
+                emoji_ui::setup_menu,
+                store_four_random_values,
+            ),
+        )
         .add_systems(Startup, emoji::load_emoji_data)
         .add_systems(
             Update,
@@ -42,6 +54,8 @@ fn main() {
                 mouse::mouse_hover,
                 mouse::hover_animation,
                 game::game_manager,
+                store_four_random_values,
+                print_random_numbers,
             ),
         )
         .run();
@@ -50,4 +64,48 @@ fn main() {
 fn setup(mut commands: Commands) {
     // Camera
     commands.spawn(Camera2dBundle::default());
+}
+
+// #[derive(Resource)]
+// struct RandomNumber(u32);
+
+// fn store_random_value(mut commands: Commands) {
+//     let mut rng = rand::thread_rng();
+//     let random_value = rng.gen_range(0..25);
+
+//     let random_number = RandomNumber(random_value);
+
+//     println!("Integer: {}", random_value);
+
+//     commands.insert_resource(random_number);
+// }
+
+// fn generate_four_random_numbers(mut commands: Commands, random_numbers: ResMut<RandomNumber>) {
+//     for _ in 0..4 {
+//         let random_number = random_numbers.0;
+
+//         println!("Generated Random Number: {}", random_number);
+//     }
+// }
+
+#[derive(Component)]
+struct RandomNumber(u32);
+
+fn store_four_random_values(mut commands: Commands) {
+    for _ in 0..4 {
+        let mut rng = rand::thread_rng();
+        let random_value = rng.gen_range(0..25);
+
+        let random_number = RandomNumber(random_value);
+
+        // println!("Generated Random Number: {}", random_value);
+
+        commands.spawn_empty().insert(random_number);
+    }
+}
+
+fn print_random_numbers(query: Query<&RandomNumber>) {
+    for random_number in query.iter() {
+        println!("Stored Random Number: {}", random_number.0);
+    }
 }
